0adf54faf01d5ff86f50cd3601535035d1a15e72
Paramita <85389813+ARNE-08@users.noreply.github.com>
2022-09-12 10:23:52 +0700

    Struct

---
+++ libft/libft_withexplaination.h
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psaengha <psaengha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/26 20:42:49 by psaengha          #+#    #+#             */
+/*   Updated: 2022/09/12 10:22:41 by psaengha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdio.h>
+
+typedef struct s_list 
+//collection of var under a single name / typedef is commonly used to simplify syntax
+//so we don't need to called struct again if we want to create a var to access struct member
+
+{
+	void			*content; //member 1
+	struct s_list	*next; //member 2
+}	t_list; //create var to allocate memory for struct and work with it
+
+//to access member
+/*ex. have char name[5], int salary
+you can access by t_list.name = "...." or t_.list.salary = 100*/ 
+
+//https://www.programiz.com/c-programming/c-structures
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
+int		ft_isprint(int c);
+int		ft_isdigit(int c);
+int		ft_strcmp(const char *s1, const char *s2);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlen(const char *s);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+char	*ft_strchr(const char *s, int c);
+char	**ft_split(char const *s, char c);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strrchr(const char *s, int c);
+int		ft_isascii(int c);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+void	*ft_memchr(const void *s, int c, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int		ft_atoi(const char *str);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putendl_fd(char *s, int fd);
+char	*ft_itoa(int n);
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+
+#endif
