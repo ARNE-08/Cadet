dfc2f66bfed0bcb99914b6b1553001a05cd8ed9d
Paramita <85389813+ARNE-08@users.noreply.github.com>
2022-09-24 23:42:15 +0700

    VALGRIND

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: psaengha <psaengha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 12:35:08 by psaengha          #+#    #+#             */
-/*   Updated: 2022/09/10 16:06:57 by psaengha         ###   ########.fr       */
+/*   Updated: 2022/09/24 22:27:37 by psaengha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
+	if (count == SIZE_MAX && size == SIZE_MAX)
+		return (0);
 	ptr = malloc(size * count);
 	if (!ptr)
-		return (0);
-	if (count == SIZE_MAX && size == SIZE_MAX)
 		return (0);
 	ft_bzero(ptr, count * size);
 	return (ptr);
